dc629fa5cc13a3b4302c923e15d5bc3bf9f2b784
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-09-19 11:41:20 +0000

    X-Center 2.05 - Multilingual Tagged Content Module

---
+++ XoopsModules/xcenter/releases/2.05/xcenter/modules/xcenter/language/english/admin.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Module: Xcenter
+
+Version: 2.01
+
+Description: Multilingual Content Module with tags and lists with search functions
+
+Author: Written by Simon Roberts aka. Wishcraft (simon@chronolabs.coop)
+
+Owner: Chronolabs
+
+License: See /docs - GPL 2.0
+*/
+
+	xoops_loadlanguage('modinfo', 'xcenter');
+	//Permissions
+	define('_XTR_NOPERMSSET', 'No Permissions to Set!');
+	define('_XTR_PERMISSIONSVIEWCATEGORY', 'View Categories!');
+	define('_XTR_VIEW_FUNCTION', 'View');
+	define('_XTR_PERMISSIONSVIEWPAGE', 'View Xcenter Page');
+	
+	define('', '');
+	define('', '');
+	define('', '');
+	define('', '');
+	define('_XTR_AM_BLOCK_TITLEA', 'Block');
+	define('_XTR_AM_BLOCK_TITLEB', 'New Block');
+	
+?>
